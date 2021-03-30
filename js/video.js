53d3b875ba0499b6a43659ac84f3871044930bb3
April <aprilts@umich.edu>
2021-03-30 16:25:58 -0400

    made new changes

--- js/video.js
+++ js/video.js
@@ -1,5 +1,15 @@
-var video;
 
+// Identify HTML Elements
+var playBtn = document.getElementById("play");
+var pauseBtn = document.getElementById("pause");
+var slowBtn = document.getElementById("slower");
+var fastBtn = document.getElementById("faster");
+var skipBtn = document.getElementById("skip");
+var muteBtn = document.getElementById("mute");
+var vintageBtn = document.getElementById("vintage");
+var originalBtn = document.getElementById("orig");
+
+// Console Logs
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
@@ -9,3 +19,38 @@ window.addEventListener("load", function() {
 // 	console.log("Play Video");
 // });
 
+var video = document.getElementById("player1");
+var volume = document.querySelector("#volume");	
+var slider = document.querySelector("#slider");
+
+	function playVid() {
+		console.log("Play Video")
+		video.play();
+	}
+
+	slider.addEventListener("change", function() {
+		video.volume = this.value / 100;
+		console.log(video.volume)
+
+		volume.innerHTML = video.volume * 100 + "%"
+
+	});
+
+	function pauseVid() {
+		console.log("Pause Video")
+		video.pause();
+	}
+
+	function setPlaySpeedSlow() {
+		video.playbackRate *=.95;
+		console.log("New speed is " + video.playbackRate)
+	}
+
+	function setPlaySpeedFast() {
+		video.playbackRate /= .95
+		console.log("New speed is " + video.playbackRate)
+	}
+
+	function setCurTime() {
+		vid.currentTime += 15;
+	}
